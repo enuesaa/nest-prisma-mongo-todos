8de16b55e203b1da0c48c49d730bed56d9965e40
nsrciog <nsrciog@gmail.com>
2022-07-23 19:58:41 +0900

    feat(todos): 基本的な Crud を定義した

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+
+  @Get()
+  list(): Array<{}> {
+    return []
+  }
+
+  @Get(':id')
+  view(@Param() params): {} {
+    return {}
+  }
+
+  @Post()
+  add(): {} {
+    return {}
+  }
+
+  @Put(':id')
+  edit(@Param() params): {} {
+    return {}
+  }
+
+  @Delete(':id')
+  delete(@Param() params): {} {
+    return {}
+  }
+}
